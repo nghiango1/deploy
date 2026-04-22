ce05b8d7da4914a5f21686b5062c8e1aedc4a428
nghiango1 <ducnghia.tin47@gmail.com>
2026-04-22 21:45:31 +0700

    fix: logger not init error

--- webhook-hanlder/cmd/webhook-handler/main.go
+++ webhook-hanlder/cmd/webhook-handler/main.go
@@ -5,11 +5,13 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/nghiango1/deploy/webhook-handler/pkg/logger"
 	"github.com/nghiango1/deploy/webhook-handler/pkg/webhook"
 )
 
 // Default http://127.0.0.1:3000/webhook
 func main() {
+	logger.SetupLogger()
 	http.HandleFunc("/webhook", webhook.Handler)
 
 	port := "3000"
