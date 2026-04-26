4c5c1a812dfb911f8514e81081600f0c859aa04f
nghiango1 <ducnghia.tin47@gmail.com>
2026-04-26 21:23:06 +0700

    feat: webhook pull deploy code

--- webhook-hanlder/pkg/webhook/push/handler_test.go
+++ webhook-hanlder/pkg/webhook/push/handler_test.go
@@ -1,11 +1,30 @@
 package push_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/nghiango1/deploy/webhook-handler/pkg/logger"
 	"github.com/nghiango1/deploy/webhook-handler/pkg/webhook/push"
 )
+
+
+func TestDevRefHandler(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+	}{
+		{"default"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := push.DevRefHandler(push.Event{})
+			if err != nil {
+				t.Errorf("Failed to pull latest code")
+			}
+		})
+	}
+}
 
 func TestMainRefHandler(t *testing.T) {
 	tests := []struct {
@@ -26,4 +45,5 @@ func TestMainRefHandler(t *testing.T) {
 
 func TestMain(m *testing.M) {
 	logger.SetupLogger()
+	os.Exit(m.Run())
 }
